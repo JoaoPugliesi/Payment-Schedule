59efbb8f21fd490f1e7c0810c157baf667c1bf9b
Joao Pugliesi <joaopaulo.allex@gmail.com>
2023-03-02 21:16:20 -0300

    first commit

---
+++ payment-schedule/src/main/java/io/github/joaopugliesi/paymentschedule/PaymentScheduleApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaopugliesi.paymentschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentScheduleApplication.class, args);
+	}
+
+}
---
+++ payment-schedule/src/test/java/io/github/joaopugliesi/paymentschedule/PaymentScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaopugliesi.paymentschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
